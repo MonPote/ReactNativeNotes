d59ab4d8b109b21a783bffada876447b3eb352ab
EAR Patrick <patrick-hk@outlook.com>
2017-08-09 14:41:33 +0200

    [TEXTINPUT] Add textinput

--- Note.js
+++ Note.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 
 export default class Note extends React.Component {
   render() {
     return (
       <View>
-        <Text> Toto </Text>
+        <TextInput
+          placeholder="test"
+        />
       </View>
     );
   }
